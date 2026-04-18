4ada8aab1d9d2db1b7ad0aaa7e87299240497d27
gabriella2939 <gabriellajoyasita@gmail.com>
2026-04-18 21:04:03 +0700

    gabby-update dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,19 @@
-// app/layout.tsx
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="id">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
       </head>
       <body>{children}</body>
     </html>
