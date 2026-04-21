601fa4d0d8d8aaecedc703ab629c73457080cd80
Yohana Griselda <yohanagayatri@gmail.com>
2026-04-21 15:27:12 +0700

    update contact-gris

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -92,6 +92,7 @@ export default function ContactPage() {
                                 src="/gambar-cp-3.png"
                                 alt="Gambar tim"
                                 fill
+                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                                 className="contact-image"
                                 priority
                             />
